7ac121561530010f7453af41ba0a8c048c7a1bf4
5hk0da <alexandrshkoda200701@gmail.com>
2026-06-28 01:00:28 +0200

    adding 28.06.2026

--- app/routers/media.py
+++ app/routers/media.py
@@ -1,4 +1,19 @@
 #API for uploading audio/video files
+#import
+from fastapi import APIRouter, UploadFile, File
 
-import json
+from app.services.audio_service import save_uploaded_file
+
+router = APIRouter()
+
+@router.post("/api/process-media")
+def process_media(file: UploadFile = File(...)):
+    file_info = save_uploaded_file(file)
+
+    return{
+        "status": "success",
+        "original_filename": file.filename,
+        "saved_as": file_info["safe_filename"],
+        "file_path": file_info["file_path"]
+    }
 
--- app/services/audio_service.py
+++ app/services/audio_service.py
@@ -1,3 +1,19 @@
 #Processing and processing audio and video
+#import
+import os
+import shutil
+from fastapi import UploadFile
 
-import 
+
+def save_uploaded_file(file: UploadFile):
+    os.makedirs("uploads", exist_ok=True)
+
+    file_path = os.path.join("uploads", file.filename)
+
+    with open(file_path, 'wb') as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "file_path": file_path,
+        "safe_filename": file.filename
+    }
