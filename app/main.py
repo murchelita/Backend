94f7427ea8ec9148d865e5fca1169c47a0fc4e6f
5hk0da <alexandrshkoda200701@gmail.com>
2026-07-09 21:15:17 +0200

    adding

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ app.include_router(youtube.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
