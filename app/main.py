9235584fcf8270731f396ddf025aea53657f7855
5hk0da <alexandrshkoda200701@gmail.com>
2026-07-16 03:15:37 +0200

    fix cors origin

--- app/main.py
+++ app/main.py
@@ -1,14 +1,16 @@
 from fastapi import FastAPI
-from starlette.middleware.cors import CORSMiddleware
-from app.routers import media, users,youtube, test, lectures
-from app.routers import quiz
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.routers import media, users, youtube, test, lectures, quiz
 
 app = FastAPI()
 
+
 origins = [
     "http://localhost:5173",
-    "https://frontend-note-quiz.vercel.app"
+    "https://frontend-note-quiz.vercel.app",
 ]
+
 
 app.add_middleware(
     CORSMiddleware,
@@ -18,23 +20,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 app.include_router(users.router)
 app.include_router(media.router)
 app.include_router(youtube.router)
 app.include_router(test.router)
-
 app.include_router(lectures.router)
-
 app.include_router(quiz.router)
+
 
 @app.get("/")
 def read_root():
     return {"Status": "working"}
-
-
-
-
-
-
-
-
