7d0f01a6c40e25a32473c52ff2f476b3a6fbd84b
5hk0da <alexandrshkoda200701@gmail.com>
2026-07-16 12:55:00 +0200

    adding

--- app/main.py
+++ app/main.py
@@ -8,20 +8,17 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",
-    "https://frontend-note-quiz.vercel.app"
+    "https://frontend-note-quiz.vercel.app",
 ]
 
-print("CORS LOADED")
-print(origins)
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
-
 
 app.include_router(users.router)
 app.include_router(media.router, prefix="/api")
